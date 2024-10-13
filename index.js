e79a3e2aad11ac7c77a1110e3c8562d599170971
Gareth Tan <garetht@users.noreply.github.com>
2024-10-13 19:05:16 -0400

    Switch domain.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var ghpages = require('gh-pages');
 
 ghpages.publish('dist', {
-        branch: 'main',
         repo: 'git@github.com:garetht/artifacts.git'
     }, () => {
     console.log("successful")
